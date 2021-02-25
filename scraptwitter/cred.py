235daad08107d7e17ea1f306e203805193c1e7ea
Anika211-R <200458857@student.georgianc.on.ca>
2021-02-24 20:41:11 -0500

    commit

---
+++ scraptwitter/cred.py
@@ -0,0 +1,15 @@
+keys = {
+    "Access token" : "1354118368509493251-u02tlah1zY31eGQgxQIq4CY6eYNQE1",
+ "Access token secret" : "4yl0v9qVeUtlt9wE1bgmwU69NJJ1oINJLk0tfkD5acCps",
+    "API key" : "CqEQG2o2Dp0qkFrZWRfY28Dso",
+    "API key secret" : "JMWNiLxqmqaffO8HNyO3iw3wfIjjCOGzmNxsmD28vOTI6pVvkf",
+    "userID" :"@AnikaRana16"
+  }
+
+redit_keys = {
+    "client_id":"I-CJguSqsOs3zw",
+    "client_secret":"gMTDWWq7VqJqVNtOPVdWJvVt6WvChw",
+    "user_agent":"script by u/Powerful_Ordinary_29",
+    "redirect_uri":"http://localhost:8080",
+    "refresh_token":"707772392715-bkNwjOnL0DWEWserTBy5z6i6WAQQyQ"
+}
